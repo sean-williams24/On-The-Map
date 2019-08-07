04f268b8208b2b16595c0e6722e1c096e003f213
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-08-07 11:26:12 +0100

    Set sign up link on login page.

--- On The Map/Controllers/LoginViewController.swift
+++ On The Map/Controllers/LoginViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
-    @IBOutlet var signUpLabel: UILabel!
+    @IBOutlet var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.center = self.view.center
         loginButton.layer.cornerRadius = 20.0
+        
+        let attributedString = NSMutableAttributedString(string: "Don't have an account? Sign Up")
+        let url = "https://www.google.com/url?q=https://www.udacity.com/account/auth%23!/signup&sa=D&ust=1565099137593000"
+        attributedString.addAttribute(.link, value: url, range: NSRange(location: 23, length: 7))
+        textView.attributedText = attributedString
+        textView.textAlignment = .center
+        
+        
+        
         
     }
 
